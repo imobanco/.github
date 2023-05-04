a3e3cc3ae6654f88a90562ac9694d6d7819512fc
Pedro <pedroalencarregis@hotmail.com>
2023-05-03 21:30:10 -0300

    Testa VMs

--- flake.nix
+++ flake.nix
@@ -221,6 +221,210 @@
         ];
     };
 
+        nixosConfigurations.nixosBuildVMX86_64LinuxDocker = let
+            pkgs = import nixpkgs {
+              system = "x86_64-linux";
+              config = { allowUnfree = true; };
+            };
+         in
+            nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        # system = "aarch64-linux";
+        modules = let
+                    nixuserKeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKyhLx5HU63zJJ5Lx4j+NTC/OQZ7Weloc8y+On467kly";
+          in [
+          "${toString (builtins.getFlake "github:NixOS/nixpkgs/a8f8b7db23ec6450e384da183d270b18c58493d4")}/nixos/modules/virtualisation/build-vm.nix"
+          "${toString (builtins.getFlake "github:NixOS/nixpkgs/a8f8b7db23ec6450e384da183d270b18c58493d4")}/nixos/modules/virtualisation/qemu-vm.nix"
+          # "${toString (builtins.getFlake "github:NixOS/nixpkgs/a8f8b7db23ec6450e384da183d270b18c58493d4")}/nixos/modules/virtualisation/qemu-guest.nix"
+          "${toString (builtins.getFlake "github:NixOS/nixpkgs/a8f8b7db23ec6450e384da183d270b18c58493d4")}/nixos/modules/installer/cd-dvd/channel.nix"
+
+          ({
+            # https://gist.github.com/andir/88458b13c26a04752854608aacb15c8f#file-configuration-nix-L11-L12
+            boot.loader.grub.extraConfig = "serial --unit=0 --speed=115200 \n terminal_output serial console; terminal_input serial console";
+            boot.kernelParams = [
+              "console=tty0"
+              "console=ttyS0,115200n8"
+              # Set sensible kernel parameters
+              # https://nixos.wiki/wiki/Bootloader
+              # https://git.redbrick.dcu.ie/m1cr0man/nix-configs-rb/commit/ddb4d96dacc52357e5eaec5870d9733a1ea63a5a?lang=pt-PT
+              "boot.shell_on_fail"
+              "panic=30"
+              "boot.panic_on_fail" # reboot the machine upon fatal boot issues
+              # TODO: test it
+              "intel_iommu=on"
+              "iommu=pt"
+
+              # https://discuss.linuxcontainers.org/t/podman-wont-run-containers-in-lxd-cgroup-controller-pids-unavailable/13049/2
+              # https://github.com/NixOS/nixpkgs/issues/73800#issuecomment-729206223
+              # https://github.com/canonical/microk8s/issues/1691#issuecomment-977543458
+              # https://github.com/grahamc/nixos-config/blob/35388280d3b06ada5882d37c5b4f6d3baa43da69/devices/petunia/configuration.nix#L36
+              # cgroup_no_v1=all
+              "swapaccount=0"
+              "systemd.unified_cgroup_hierarchy=0"
+              "group_enable=memory"
+            ];
+
+            boot.tmpOnTmpfs = false;
+            # https://github.com/AtilaSaraiva/nix-dotfiles/blob/main/lib/modules/configHost/default.nix#L271-L273
+            boot.tmpOnTmpfsSize = "100%";
+
+            # https://nixos.wiki/wiki/NixOS:nixos-rebuild_build-vm
+            users.extraGroups.nixgroup.gid = 999;
+
+            users.users.nixuser = {
+              isSystemUser = true;
+              password = "1";
+              createHome = true;
+              home = "/home/nixuser";
+              homeMode = "0700";
+              description = "The VM tester user";
+              group = "nixgroup";
+              extraGroups = [
+                              "docker"
+                              "kvm"
+                              "libvirtd"
+                              "wheel"
+              ];
+              packages = with pkgs; [
+                  direnv
+                  gitFull
+                  xorg.xclock
+                  file
+                  # pkgsCross.aarch64-multiplatform-musl.pkgsStatic.hello
+                  btop
+                  # firefox
+                  # vscode
+                  # (python3.buildEnv.override
+                  #   {
+                  #     extraLibs = with python3Packages; [ scikitimage opencv2 numpy ];
+                  #   }
+                  # )
+              ];
+              shell = pkgs.bashInteractive;
+              uid = 1234;
+              autoSubUidGidRange = true;
+
+              openssh.authorizedKeys.keyFiles = [
+                "${ ./nixuser-keys.pub }"
+              ];
+
+              openssh.authorizedKeys.keys = [
+                 "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKyhLx5HU63zJJ5Lx4j+NTC/OQZ7Weloc8y+On467kly"
+              ];
+            };
+
+              systemd.services.adds-change-workdir = {
+                script = "echo cd /tmp/shared >> /home/nixuser/.profile";
+                wantedBy = [ "multi-user.target" ];
+              };
+
+              systemd.services.creates-if-not-exist = {
+                script = "echo touch /home/nixuser/.Xauthority >> /home/nixuser/.profile";
+                wantedBy = [ "multi-user.target" ];
+              };
+
+              # https://unix.stackexchange.com/questions/619671/declaring-a-sym-link-in-a-users-home-directory#comment1159159_619703
+              systemd.services.populate-history = {
+                script = "echo \"ls -al /nix/store\" >> /home/nixuser/.bash_history";
+                wantedBy = [ "multi-user.target" ];
+              };
+
+              virtualisation = {
+                # following configuration is added only when building VM with build-vm
+                memorySize = 3072; # Use MiB memory.
+                diskSize = 4096; # Use MiB memory.
+                cores = 7;         # Simulate 3 cores.
+                #
+                docker.enable = true;
+
+                #
+                useNixStoreImage = true;
+                writableStore = true; # TODO
+              };
+              security.polkit.enable = true;
+
+            environment.etc."containers/registries.conf" = {
+              mode = "0644";
+              text = ''
+                [registries.search]
+                registries = ['docker.io', 'localhost']
+              '';
+            };
+
+              # https://nixos.wiki/wiki/Libvirt
+              boot.extraModprobeConfig = "options kvm_intel nested=1";
+              boot.kernelModules = [
+                "kvm-intel"
+                "vfio-pci"
+              ];
+
+              # hardware.opengl.enable = true;
+              # hardware.opengl.driSupport = true;
+
+              nixpkgs.config.allowUnfree = true;
+              nix = {
+                package = pkgs.nix;
+                # package = pkgsCross.aarch64-multiplatform-musl.pkgsStatic.nix;
+                extraOptions = "experimental-features = nix-command flakes";
+                readOnlyStore = true;
+              };
+
+              boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+              # Enable the X11 windowing system.
+              services.xserver = {
+                enable = true;
+                displayManager.gdm.enable = true;
+                displayManager.startx.enable = true;
+                logFile = "/var/log/X.0.log";
+                desktopManager.xterm.enable = true;
+                # displayManager.gdm.autoLogin.enable = true;
+                # displayManager.gdm.autoLogin.user = "nixuser";
+              };
+              services.spice-vdagentd.enable = true;
+
+              # https://github.com/NixOS/nixpkgs/issues/21332#issuecomment-268730694
+              services.openssh = {
+                allowSFTP = true;
+                kbdInteractiveAuthentication = false;
+                enable = true;
+                forwardX11 = true;
+                passwordAuthentication = false;
+                permitRootLogin = "yes";
+                ports = [ 10022 ];
+                authorizedKeysFiles = [
+                   "${ ./nixuser-keys.pub }"
+                ];
+              };
+
+              # https://stackoverflow.com/a/71247061
+              # https://nixos.wiki/wiki/Firewall
+              # networking.firewall = {
+              #   enable = true;
+              #   allowedTCPPorts = [ 22 80 443 10022 8000 ];
+              # };
+
+              programs.ssh.forwardX11 = true;
+              services.qemuGuest.enable = true;
+
+              services.sshd.enable = true;
+
+              programs.dconf.enable = true;
+
+              time.timeZone = "America/Recife";
+            system.stateVersion = "22.11";
+
+            users.users.root = {
+              password = "root";
+              initialPassword = "root";
+              openssh.authorizedKeys.keyFiles = [
+                 "${ ./nixuser-keys.pub }"
+              ];
+            };
+          })
+        ];
+    };
+
         nixosConfigurations.nixosBuildVMAarch64Linux = let
             pkgs = import nixpkgs {
               # system = "x86_64-linux";
--- profile/README.md
+++ profile/README.md
@@ -778,7 +778,23 @@ EOF
 chmod -v 0600 id_ed25519
 
 
-result/bin/run-nixos-vm < /dev/null &
+nix \
+--option eval-cache false \
+--option extra-trusted-public-keys binarycache-1:XiPHS/XT/ziMHu5hGoQ8Z0K88sa1Eqi5kFTYyl33FJg= \
+--option extra-substituters "s3://playing-bucket-nix-cache-test" \
+build \
+--keep-failed \
+--max-jobs 0 \
+--no-link \
+--no-show-trace \
+--print-build-logs \
+--print-out-paths \
+github:PedroRegisPOAR/.github/5ef51c7cc275a4298c8870e73dddf40ca39aa04c#nixosConfigurations.x86_64-linux.nixosBuildVMX86_64Linux.config.system.build.vm
+
+nix \
+run \
+github:PedroRegisPOAR/.github/5ef51c7cc275a4298c8870e73dddf40ca39aa04c#nixosConfigurations.x86_64-linux.nixosBuildVMX86_64Linux.config.system.build.vm \
+< /dev/null &
 
 
 while ! ssh -i id_ed25519 -o ConnectTimeout=1 -o StrictHostKeyChecking=no nixuser@localhost -p 10022 <<<'nix flake metadata nixpkgs'; do \
@@ -790,11 +806,123 @@ ssh \
 -o StrictHostKeyChecking=no \
 nixuser@localhost \
 -p 10022
+
 #<<COMMANDS
 #id
 #COMMANDS
 #"$REMOVE_DISK" && rm -fv nixos.qcow2 id_ed25519
 ```
+
+
+#### x86_64-linux with docker
+
+Docker
+
+```bash
+nix \
+build \
+--max-jobs auto \
+--no-link \
+--no-show-trace \
+--print-build-logs \
+#nixosConfigurations.x86_64-linux.nixosBuildVMX86_64LinuxDocker.config.system.build.vm
+```
+
+```bash
+nix \
+build \
+--max-jobs auto \
+--no-link \
+--no-show-trace \
+--print-build-logs \
+github:PedroRegisPOAR/.github/5ef51c7cc275a4298c8870e73dddf40ca39aa04c#nixosConfigurations.x86_64-linux.nixosBuildVMX86_64LinuxDocker.config.system.build.vm
+
+send-signed-closure-run-time-of-flake-uri-attr-to-bucket \
+github:PedroRegisPOAR/.github/5ef51c7cc275a4298c8870e73dddf40ca39aa04c#nixosConfigurations.x86_64-linux.nixosBuildVMX86_64LinuxDocker.config.system.build.vm
+```
+
+
+```bash
+time \
+nix \
+--option eval-cache false \
+--option extra-trusted-public-keys binarycache-1:XiPHS/XT/ziMHu5hGoQ8Z0K88sa1Eqi5kFTYyl33FJg= \
+--option extra-substituters "s3://playing-bucket-nix-cache-test" \
+build \
+--keep-failed \
+--max-jobs 0 \
+--no-link \
+--no-show-trace \
+--print-build-logs \
+--print-out-paths \
+github:PedroRegisPOAR/.github/5ef51c7cc275a4298c8870e73dddf40ca39aa04c#nixosConfigurations.x86_64-linux.nixosBuildVMX86_64LinuxDocker.config.system.build.vm
+```
+
+
+```bash
+mkdir -pv ~/sandbox/sandbox && cd $_
+
+export HOST_MAPPED_PORT=10022
+export REMOVE_DISK=true
+export QEMU_NET_OPTS='hostfwd=tcp::10022-:10022,hostfwd=tcp:127.0.0.1:8000-:8000'
+export QEMU_OPTS='-nographic'
+export SHARED_DIR="$(pwd)"
+
+"$REMOVE_DISK" && rm -fv nixos.qcow2
+# nc 1>/dev/null 2>/dev/null || nix profile install nixpkgs#netcat
+# nc -v -4 localhost "$HOST_MAPPED_PORT" -w 1 -z && echo 'There is something already using the port:'"$HOST_MAPPED_PORT"
+
+# sudo lsof -t -i tcp:10022 -s tcp:listen
+# sudo lsof -t -i tcp:10022 -s tcp:listen | sudo xargs --no-run-if-empty kill
+
+cat << 'EOF' >> id_ed25519
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACCsoS8eR1Ot8ySeS8eI/jUwvzkGe1npaHPMvjp+Ou5JcgAAAIjoIwah6CMG
+oQAAAAtzc2gtZWQyNTUxOQAAACCsoS8eR1Ot8ySeS8eI/jUwvzkGe1npaHPMvjp+Ou5Jcg
+AAAEAbL0Z61S8giktfR53dZ2fztctV/0vML24doU0BMGLRZqyhLx5HU63zJJ5Lx4j+NTC/
+OQZ7Weloc8y+On467klyAAAAAAECAwQF
+-----END OPENSSH PRIVATE KEY-----
+EOF
+
+chmod -v 0600 id_ed25519
+
+
+nix \
+--option eval-cache false \
+--option extra-trusted-public-keys binarycache-1:XiPHS/XT/ziMHu5hGoQ8Z0K88sa1Eqi5kFTYyl33FJg= \
+--option extra-substituters "s3://playing-bucket-nix-cache-test" \
+build \
+--keep-failed \
+--max-jobs 0 \
+--no-link \
+--no-show-trace \
+--print-build-logs \
+--print-out-paths \
+github:PedroRegisPOAR/.github/5ef51c7cc275a4298c8870e73dddf40ca39aa04c#nixosConfigurations.x86_64-linux.nixosBuildVMX86_64LinuxDocker.config.system.build.vm
+
+nix \
+run \
+github:PedroRegisPOAR/.github/5ef51c7cc275a4298c8870e73dddf40ca39aa04c#nixosConfigurations.x86_64-linux.nixosBuildVMX86_64LinuxDocker.config.system.build.vm \
+< /dev/null &
+
+
+while ! ssh -i id_ed25519 -o ConnectTimeout=1 -o StrictHostKeyChecking=no nixuser@localhost -p 10022 <<<'nix flake metadata nixpkgs'; do \
+  echo $(date +'%d/%m/%Y %H:%M:%S:%3N'); sleep 0.5; done \
+&& ssh-keygen -R '[localhost]:10022'; \
+ssh \
+-i id_ed25519 \
+-X \
+-o StrictHostKeyChecking=no \
+nixuser@localhost \
+-p 10022
+
+#<<COMMANDS
+#id
+#COMMANDS
+#"$REMOVE_DISK" && rm -fv nixos.qcow2 id_ed25519
+```
+
 
 
 #### aarch64-linux
@@ -859,7 +987,12 @@ EOF
 chmod -v 0600 id_ed25519
 
 
-result/bin/run-nixos-vm < /dev/null &
+
+
+nix \
+run \
+github:PedroRegisPOAR/.github/991bde1c67c86bc382601c01b2cb7dd6754c953e#nixosConfigurations.x86_64-linux.nixosBuildVMAarch64Linux.config.system.build.vm \
+< /dev/null &
 
 
 while ! ssh -i id_ed25519 -o ConnectTimeout=1 -o StrictHostKeyChecking=no nixuser@localhost -p 10022 <<<'nix flake metadata nixpkgs'; do \
@@ -871,6 +1004,7 @@ ssh \
 -o StrictHostKeyChecking=no \
 nixuser@localhost \
 -p 10022
+
 #<<COMMANDS
 #id
 #COMMANDS
@@ -888,10 +1022,19 @@ strace -f -T -y -e trace=file
 ```bash
 export DOCKER_HOST=ssh://nixuser@localhost:10022
 
-podman run -p8080:80 nginx
+docker run -p8080:80 nginx
 
 
-ssh -L 8000:localhost:8000 -p 10022 nixuser@localhost
+ssh -L 8080:localhost:8080 -p 10022 nixuser@localhost
 ```
 Refs.:
 - https://dev.to/jillesvangurp/docker-over-qemu-on-a-mac-1ajp
+
+
+
+
+```bash
+podman --remote --identity id_ed25519 --url ssh://nixuser@localhost:10022 images
+```
+Refs.:
+- https://stackoverflow.com/a/74634171
