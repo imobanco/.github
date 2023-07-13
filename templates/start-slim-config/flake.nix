7abac2b97373e3ba14296751bd515a0b4895f562
Pedro <pedroalencarregis@hotmail.com>
2023-07-13 20:09:40 -0300

    registry.nixpkgs.flake = nixpkgs; + sessionVariables.NIX_PATH = "nixpkgs=${nixpkgs.outPath}";

--- templates/start-slim-config/flake.nix
+++ templates/start-slim-config/flake.nix
@@ -33,7 +33,7 @@
               stateVersion = "22.11";
               # https://discourse.nixos.org/t/correct-way-to-use-nixpkgs-in-nix-shell-on-flake-based-system-without-channels/19360/3
               # sessionVariables.NIX_PATH = "nixpkgs=nixpkgs=flake:?";
-              sessionVariables.NIX_PATH = "nixpkgs=${pkgs.outPath}";
+              sessionVariables.NIX_PATH = "nixpkgs=${nixpkgs.outPath}";
             };
             programs.home-manager.enable = true;
           }
