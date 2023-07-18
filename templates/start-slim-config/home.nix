7dfd6e8a390669c57046239ded9962f8a69706b6
Pedro <pedroalencarregis@hotmail.com>
2023-07-18 09:51:09 -0300

    Aumenta para 1 ano tarball-ttl

--- templates/start-slim-config/home.nix
+++ templates/start-slim-config/home.nix
@@ -495,7 +495,7 @@
       keep-outputs = true;
       keep-derivations = true;
 
-      tarball-ttl = 60 * 60 * 24 * 7 * 4; # = 2419200 = one month
+      tarball-ttl = 60 * 60 * 24 * 7 * 4 * 12; # = 2419200 * 12 = 12 months
       # readOnlyStore = true;
 
       # trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
